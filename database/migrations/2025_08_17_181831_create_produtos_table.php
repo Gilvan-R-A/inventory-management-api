11c7cd3e8efb6ea75b0e001b6cb42f09670573d4
Gilvan-R-A <gilvan.dev@gmail.com>
2025-08-17 15:48:11 -0300

    feat: adiciona migrations para tabela de produtos

---
+++ database/migrations/2025_08_17_181831_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku', 50)->unique();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->string('categoria')->nullable();
+            $table->decimal('preco', 10, 2)->default(0);
+            $table->string('fornecedor')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
